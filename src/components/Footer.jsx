1d39d176ea1a7d15c2e7ea92b7140dcaeeae5329
Harshit Agarwal <harshitagarwal713@gmail.com>
2024-09-14 02:30:24 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         <div className="footer-heading">
             <h1 className='footer-heading-h1'>STAY UPDATED</h1>
             <p className='footer-heading-para'>Sign up to our newsletter and hear about the big ideas and new campaigns, taking place all around the world, that are helping to drive progress towards the Global Goals.</p>
-            <h2>Created By Team Developers </h2>
+            <h2>Created By Team "Developers" </h2>
             <div className="icons">
                 <a href="https://www.youtube.com/channel/UCRfuAYy7MesZmgOi1Ezy0ng"><FontAwesomeIcon icon={faYoutube} className='fa-face' /></a>
                 <a href="https://www.instagram.com/TheGlobalGoals/"><FontAwesomeIcon icon={faInstagram} className='fa-insta' /></a>
