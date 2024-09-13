66eeafff573c902219162073ae0b5e198b3838e9
harshitagarwal04 <harshitagarwal713@gmail.com>
2024-09-13 17:48:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section0 from './components/Section0';
+import Section3 from './components/Section3';
+import Section4 from './components/Section4';
+import Footer from './components/Footer';
+
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Section1 />
+      <Section0 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Section0.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Section0() {
+
+    return (
+        <span className='main'>
+        <section className="section section0">
+            <h2 className='section0-heading'>CONSERVE AND SUSTAINABLY USE THE OCEANS, SEAS AND MARINE RESOURCES FOR SUSTAINABLE DEVELOPMENT.
+            </h2>
+            <div className='section0-para' >
+                <p>Healthy oceans and seas are essential to our existence.They cover 70 percent of our planet <br />and we rely on them for food, energy and water. Yet, we have managed to do tremendous damage to these precious resources. We must protect them by eliminating pollution and overfishing and immediately start to responsibly manage and protect all marine life around the world.
+                </p>
+            <div className='button1'>
+             <a href="https://www.globalgoals.org/resources/"><button>RESOURCE</button> </a>
+             <a href="https://www.globalgoals.org/take-action/?id==14"> <button>TAKE ACTION</button> </a>  
+             <a href="https://www.globalgoals.org/goals/14-life-below-water/#things-to-do"><button>THINGS TO DO</button> </a>
+            </div>
+            </div>
+        </section>
+        </span>
+    );
+}
+
+export default Section0;
